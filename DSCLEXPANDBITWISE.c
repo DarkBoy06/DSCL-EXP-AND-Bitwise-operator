970537ec8fb4824a3a8e584e7cb898a94b3f77ed
Anish Sawhney <76051087+DarkBoy06@users.noreply.github.com>
2023-04-26 09:47:22 +0530

    Add files via upload

---
+++ DSCLEXPANDBITWISE.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int x;
+	printf("Insert Number 1:  ");
+	scanf("%d",&x);
+	int y;
+	printf("Insert Number 2:  ");
+	scanf("%d",&y);
+
+	printf("Output = %d", x & y);
+
+return 0;
+}
